f398036d6d86760f03511adcc1b0a1aec42090e7
ShaviyaVictor <shaviyavictor@gmail.com>
2022-03-05 15:56:27 +0300

    Redirect the new user upon registration

--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 
@@ -15,6 +15,8 @@ def register(request) :
       username = form.cleaned_data.get('username')
 
       messages.success(request, f'Account successfully created for {username}!')
+
+      return redirect('blog-home')
 
   else :
     form = UserCreationForm()
