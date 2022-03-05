50ba74ec40bf7d403dddc085be9e3976fbd3e7dc
ShaviyaVictor <shaviyavictor@gmail.com>
2022-03-05 07:39:53 +0300

    Call in variables to the templates using variables

--- blog/views.py
+++ blog/views.py
@@ -2,16 +2,46 @@ from django.shortcuts import render
 
 
 
+
+# Dummy data to pass it as variable
+
+posts = [
+  {
+
+    'author': 'Victor Shaviya',
+    'title': 'Adventure',
+    'caption': 'Huh! You must have enjoyed',
+    'date_posted': 'Aug 10, 1996'
+
+  },
+  {
+
+    'author': 'Josphine Mbaisi',
+    'title': 'Voyage',
+    'caption': 'The journey was indeed very long',
+    'date_posted': 'March 10, 2018'
+
+  },
+]
+
 # Create your views here.
 def home(request) :
 
-  return render(request, 'blog/home.html')
+  context = {
+    'posts': posts,
+    'title': 'Home'
+  }
+
+  return render(request, 'blog/home.html', context)
+
 
 
 
 def messenger(request) :
 
-  return render(request, 'blog/messenger.html')
+
+
+  return render(request, 'blog/messenger.html', {'title':'Messenger'})
 
 
 
