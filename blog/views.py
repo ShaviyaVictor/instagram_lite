fcddffe78341354ec916fe71cab6e4c836ac7ce7
ShaviyaVictor <shaviyavictor@gmail.com>
2022-03-05 13:33:56 +0300

    Rgister the Post model

--- blog/views.py
+++ blog/views.py
@@ -4,27 +4,6 @@ from .models import Post
 
 
 
-# Dummy data to pass it as variable
-
-posts = [
-  {
-
-    'author': 'Victor Shaviya',
-    'title': 'Adventure',
-    'caption': 'Huh! You must have enjoyed',
-    'date_posted': 'Aug 10, 1996'
-
-  },
-  {
-
-    'author': 'Josphine Mbaisi',
-    'title': 'Voyage',
-    'caption': 'The journey was indeed very long',
-    'date_posted': 'March 10, 2018'
-
-  },
-]
-
 # Create your views here.
 def home(request) :
 
@@ -41,7 +20,7 @@ def home(request) :
 def messenger(request) :
 
   context = {
-      'posts': posts,
+      
       'title': 'Messenger'
     }
 
