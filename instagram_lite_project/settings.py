6b60992de1fa8fdc38a8cbc6477f26358cc16eec
ShaviyaVictor <shaviyavictor@gmail.com>
2022-03-05 16:30:28 +0300

    Install crispy module and register it to help with styling the form

--- instagram_lite_project/settings.py
+++ instagram_lite_project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'users.apps.UsersConfig',
+    'crispy_forms',
 
     'django.contrib.admin',
     'django.contrib.auth',
@@ -124,3 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
