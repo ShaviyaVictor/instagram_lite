71c2cb5af2b5cf9b1f7e777b78c3964418efe8f3
ShaviyaVictor <shaviyavictor@gmail.com>
2022-03-05 12:47:05 +0300

    Create the initial post model db

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
+from tkinter import CASCADE
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+
+
+
+
 
 # Create your models here.
+class Post(models.Model) :
+  img_title = models.CharField(max_length=100)
+  img_caption = models.CharField(max_length=500)
+  profile = models.ForeignKey(User, on_delete=models.CASCADE)
+  date_posted = models.DateTimeField(default=timezone.now)
+
