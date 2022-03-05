871bdbb0297d8fa5e57559b80dde755b7d8eadba
ShaviyaVictor <shaviyavictor@gmail.com>
2022-03-05 15:52:12 +0300

    Register the POST method to capture cleaned data from the form

--- users/views.py
+++ users/views.py
@@ -6,7 +6,15 @@ from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
 
 def register(request) :
-  form = UserCreationForm()
+
+  if request.method == 'POST' :
+
+    form = UserCreationForm(request.POST)
+    if form.is_valid() :
+      username = form.cleaned_data.get('username')
+
+  else :
+    form = UserCreationForm()
 
 
   return render(request, 'users/register.html', {'form': form})
