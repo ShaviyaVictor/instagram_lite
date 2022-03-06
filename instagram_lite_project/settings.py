3f8c882af33e4fc6159018a54c5637a4e399636f
ShaviyaVictor <shaviyavictor@gmail.com>
2022-03-06 16:22:16 +0300

    Declare the MEDIA_ROOT paths

--- instagram_lite_project/settings.py
+++ instagram_lite_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -122,6 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import NewsLetterRecipients
+from .models import NewsLetterRecipients, Profile
 
 
 # Register your models here.
 admin.site.register(NewsLetterRecipients)
+admin.site.register(Profile)
