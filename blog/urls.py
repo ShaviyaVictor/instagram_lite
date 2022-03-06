3785f27e46947cc26ced6cd1fc45794883d461c9
ShaviyaVictor <shaviyavictor@gmail.com>
2022-03-06 20:53:07 +0300

    Create the Post creation form in the post_form.html template

--- blog/urls.py
+++ blog/urls.py
@@ -9,7 +9,10 @@ urlpatterns = [
     # path('', views.home, name='blog-home'),
     path('', PostListView.as_view(), name='blog-home'),
 
+    # template name is post_detail.html
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    # template name is post_form.html
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
 
     path('messenger/', views.messenger, name='blog-messenger'),
     path('post/', views.add_post, name='blog-post'),
