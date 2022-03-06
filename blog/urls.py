e94f0e2304f5159b5f5b1fb50128cbb741ee1e29
ShaviyaVictor <shaviyavictor@gmail.com>
2022-03-06 20:25:11 +0300

    Implement the List View functionality

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
+from .views import PostListView
 from . import views
 
 
 
 urlpatterns = [
 
-    path('', views.home, name='blog-home'),
+    # path('', views.home, name='blog-home'),
+    path('', PostListView.as_view(), name='blog-home'),
     path('messenger/', views.messenger, name='blog-messenger'),
     path('post/', views.add_post, name='blog-post'),
     
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Post
 from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView
 
 
 
@@ -15,6 +16,15 @@ def home(request) :
   }
 
   return render(request, 'blog/home.html', context)
+
+
+
+class PostListView(ListView) :
+
+  model = Post
+
+  template_name = 'blog/home.html'
+  context_object_name = 'posts'
 
 
 
