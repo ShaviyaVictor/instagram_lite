a8dfc437c0de38c50bbd3605d7d7476a09a03631
ShaviyaVictor <shaviyavictor@gmail.com>
2022-03-06 20:31:43 +0300

    Passing variables to the url patterns

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import PostListView
+from .views import PostListView, PostDetailView
 from . import views
 
 
@@ -8,6 +8,9 @@ urlpatterns = [
 
     # path('', views.home, name='blog-home'),
     path('', PostListView.as_view(), name='blog-home'),
+
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+
     path('messenger/', views.messenger, name='blog-messenger'),
     path('post/', views.add_post, name='blog-post'),
     
