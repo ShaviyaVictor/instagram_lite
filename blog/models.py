25d9525ec0221156a9610162474eb03f67bac17c
ShaviyaVictor <shaviyavictor@gmail.com>
2022-03-06 19:27:53 +0300

    Create the Comment model

--- blog/models.py
+++ blog/models.py
@@ -30,3 +30,8 @@ class Post(models.Model) :
 
 
 
+class Comment(models.Model) :
+  comment = models.TextField()
+  created_on = models.DateTimeField(default=timezone.now)
+  author = models.ForeignKey(User, on_delete=models.CASCADE)
+  post = models.ForeignKey('Post', on_delete=models.CASCADE)
