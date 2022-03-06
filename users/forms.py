6d65c6b68130ba140f13856f3d0111a7c73606d6
ShaviyaVictor <shaviyavictor@gmail.com>
2022-03-06 17:19:20 +0300

    Create the UserUpdateForm for updating the user details from the frontend

--- users/forms.py
+++ users/forms.py
@@ -2,6 +2,7 @@ from dataclasses import fields
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 
 class UserRegisterForm(UserCreationForm) :
@@ -15,5 +16,18 @@ class UserRegisterForm(UserCreationForm) :
       'password1',
       'password2',
     ]
+
+
+
+class UserUpdateForm(forms.ModelForm) :
+  email = forms.EmailField()
+  
+  class Meta :
+    model = User
+    fields = [
+      'username',
+      'email',
+    ]
+
 
 
